3a26b14500636640f2d9741baa64937db9fad75a
yasuonagaro <yasuo.nagaro@gmail.com>
2025-10-18 17:56:09 +0900

    commit

---
+++ app/src/main/java/jp/techacademy/yasuo/nagaro/qa_app/Answer.kt
@@ -0,0 +1,6 @@
+package jp.techacademy.yasuo.nagaro.qa_app
+
+import java.io.Serializable
+
+class Answer(val body: String, val name: String, val uid: String, val answerUid: String) :
+    Serializable
